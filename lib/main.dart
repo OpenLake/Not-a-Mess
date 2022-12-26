779abe6611a075b558adc249061b1bc2133c4500
HridayAg0102 <hridayagrawal0102@gmail.com>
2022-12-27 00:32:09 +0530

    FIX: cleared some error

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 import 'package:flutter/material.dart';
+import 'package:not_a_mess/registration.dart';
 import 'package:not_a_mess/widgets/chip_builder.dart';
 import 'package:not_a_mess/widgets/choice_chip_buider.dart';
 import 'package:not_a_mess/widgets/star_rating.dart';
@@ -24,58 +25,10 @@ void main() {
               fontSize: 16.0, color: Colors.white, fontFamily: 'Hind'),
         ),
       ),
-      home: MyApp(),
+      home: Register(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 150,
-            ),
-            Container(
-              child: Text(
-                "NARIYAL CHUTNEY",
-                style: TextStyle(
-                  color: primaryColor,
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 0.18*(MediaQuery. of(context). size. width)),
-              child: Rating(),
-            ),
-
-            SizedBox(
-              height: 50,
-            ),
-
-            Wrap(spacing: 8, children: <Widget>[
-              ChoiceChipBuild("undercooked"),
-              ChoiceChipBuild("too oily"),
-              ChoiceChipBuild("too spicy"),
-              ChoiceChipBuild("overcooked"),
-              ChoiceChipBuild("needs more salt"),
-              ChoiceChipBuild("stale")
-            ]),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
---
+++ lib/registration.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'constants/colors.dart';
+
+class Register extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Container(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        child: Column(
+
+          children: <Widget>[
+            SizedBox(height: 0.23*(MediaQuery. of(context). size. height),),
+            Text("Register", style: TextStyle(color: primaryColor, fontSize: 50),),
+            SizedBox(height: 25,),
+            TextButton.icon(
+                onPressed: (){},
+                icon: Icon(Icons.school, color: backgroundColor,),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 35),
+                  backgroundColor: Colors.white,
+                ),
+                label: Text("Student", style: TextStyle(color: backgroundColor, fontSize: 32),)
+            ),
+            SizedBox(height: 15,),
+            TextButton.icon(
+                onPressed: (){},
+                icon: Icon(Icons.food_bank, color: backgroundColor,),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.all(8),
+                  backgroundColor: Colors.white,
+                ),
+                label: Text("Maintainer", style: TextStyle(color: backgroundColor, fontSize: 32),)
+            ),
+            SizedBox(height: 0.15*(MediaQuery. of(context). size. height)),
+            Image(image: AssetImage("./assets/logo.png"), color: Colors.white, height: 0.15*(MediaQuery. of(context). size. height),),
+            Text("Not-A-Mess", style: TextStyle(color: Colors.white, fontSize: 0.07*(MediaQuery. of(context). size. width)),)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
