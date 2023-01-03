3c2482ee586326dc2dc768e7d425dea0bdd1b667
HridayAg0102 <hridayagrawal0102@gmail.com>
2023-01-03 21:40:57 +0530

    Need debugging

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:not_a_mess/registration.dart';
 import 'package:not_a_mess/widgets/chip_builder.dart';
 import 'package:not_a_mess/widgets/choice_chip_buider.dart';
+import 'package:not_a_mess/widgets/dashboard_widgets/bar_chart.dart';
+import 'package:not_a_mess/widgets/dashboard_widgets/vertical_bar_chart.txt';
 import 'package:not_a_mess/widgets/star_rating.dart';
 import 'constants/colors.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(
@@ -25,10 +28,53 @@ void main() {
               fontSize: 16.0, color: Colors.white, fontFamily: 'Hind'),
         ),
       ),
-      home: Register(),
+      home: MyApp(),
     ),
   );
 }
 
+class MyApp extends StatelessWidget {
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(5),
+        child: Container(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 30,),
+              Text(
+                "Dashboard",
+                style: TextStyle(color: primaryColor, fontSize: 40),
+              ),
+              SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ChipBuild("Day", fontsize: 24),
+                  ChipBuild("Week", fontsize: 24),
+                  ChipBuild("Month", fontsize: 24),
+                  ChipBuild("Year", fontsize: 24),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 0.16*(MediaQuery. of(context). size. width)),
+                child: Rating(),
+              ),
+            Container(
+                height: double.maxFinite,
+                width: double.maxFinite,
+                  child: BarChartWidget()
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/chip_builder.dart
+++ lib/widgets/chip_builder.dart
@@ -1,14 +1,21 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:not_a_mess/constants/colors.dart';
 
 class ChipBuild extends StatelessWidget {
   final String name;
-  const ChipBuild(this.name);
+  final double? fontsize;
+  const ChipBuild(this.name, {this.fontsize});
 
   @override
   Widget build(BuildContext context) {
+    double? defaultFontSize = 17.0;
+    if (fontsize != null){
+      defaultFontSize = fontsize;
+    };
     return Chip(
-      label: Text(name, style: TextStyle(color: Colors.white, fontSize: 17),),
+      label: Text(name, style: TextStyle(color: Colors.white, fontSize: defaultFontSize),),
       backgroundColor: backgroundColor,
       shape: const StadiumBorder(side: BorderSide(color: Colors.white)),
       padding: const EdgeInsets.all(4),
